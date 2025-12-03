1904aa771b77fd8419364da422616448d522b782
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-12-03 21:53:07 +0530

    Add result calculation for Rock Paper Scissors game
    Introduced a Result function to determine the outcome based on user and computer choices. Refactored Computer and Picture functions to support result calculation and display the game result to the user.

--- Projects/1 Rock Paper Scissor Scissor/Game.js
+++ Projects/1 Rock Paper Scissor Scissor/Game.js
@@ -1,6 +1,6 @@
 const Computer = () => {
-        let num = Math.floor(Math.random() * 3) + 1;
 
+        let num = Math.floor(Math.random() * 3) + 1;
         if (num === 1) {
         document.getElementById("imgcom").src = "assets/stone.png";
         document.getElementById("para").innerText = "Stone";
@@ -13,10 +13,11 @@ const Computer = () => {
         document.getElementById("imgcom").src = "assets/scissor.png";
         document.getElementById("para").innerText = "Scissor";
     }
+    return num;
 }
 
 const Picture = (Choice) => {
-    
+
     if (Choice === 'stone') {
         document.getElementById("imguser").src = "assets/stone.png";
 
@@ -27,5 +28,46 @@ const Picture = (Choice) => {
         document.getElementById("imguser").src = "assets/scissor.png";
     }
     Computer();
+    Result();
+
+    return Choice;
+}
+
+const Result = (user) => {
+
+    const num = Computer(); 
+    Result(user, num);
+
+    let press = Picture(user);
+    if (press === 'stone') {
+        if (num === 1) {
+            document.getElementById("res").innerText="Match draw..!!";
+        } else if (num === 2) {
+            document.getElementById("res").innerText="You Won..!!";
+        }
+        else if (num === 3) {
+            document.getElementById("res").innerText="You Lose..!!";
+        }
+    }    
+    if (press === 'paper') {
+        if (num === 1) {
+            document.getElementById("res").innerText="You Won..!!";
+        } else if (num === 2) {
+            document.getElementById("res").innerText="Match Draw..!!";
+        }
+        else if (num === 3) {
+            document.getElementById("res").innerText="You Lose..!!";
+        }
+    }
+    if (press === 'scissor') {
+        if (num === 1) {
+            document.getElementById("res").innerText="You Lose..!!";
+        } else if (num === 2) {
+            document.getElementById("res").innerText="You Won..!!";
+        }
+        else if (num === 3) {
+            document.getElementById("res").innerText="Match Draw..!!";
+        }
+    }
 }
 
