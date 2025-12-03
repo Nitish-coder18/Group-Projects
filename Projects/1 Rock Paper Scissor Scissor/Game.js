1038a1703bc05f85cf59cc2058808798f0e95c66
Nitish Dwivedi <nitishdivedi16@gmail.com>
2025-12-03 19:57:59 +0530

    Refactor computer choice logic and update UI elements

--- Projects/1 Rock Paper Scissor Scissor/Game.js
+++ Projects/1 Rock Paper Scissor Scissor/Game.js
@@ -1,3 +1,20 @@
+const Computer = () => {
+        let num = Math.floor(Math.random() * 3) + 1;
+
+        if (num === 1) {
+        document.getElementById("imgcom").src = "assets/stone.png";
+        document.getElementById("para").innerText = "Stone";
+
+        } else if (num === 2) {
+        document.getElementById("imgcom").src = "assets/scroll.png";
+        document.getElementById("para").innerText = "Paper";
+
+    } else if (num === 3) {
+        document.getElementById("imgcom").src = "assets/scissor.png";
+        document.getElementById("para").innerText = "Scissor";
+    }
+}
+
 const Picture = (Choice) => {
     
     if (Choice === 'stone') {
@@ -9,20 +26,6 @@ const Picture = (Choice) => {
     } else if (Choice === 'scissor') {
         document.getElementById("imguser").src = "assets/scissor.png";
     }
-
-    const Computer = () => {
-        let num = Math.floor(Math.random() * 3) + 1;
-
-        if (num === 1) {
-        document.getElementById("imguser").src = "assets/stone.png";
-
-        } else if (num === 2) {
-        document.getElementById("imguser").src = "assets/scroll.png";
-
-    } else if (num === 3) {
-        document.getElementById("imguser").src = "assets/scissor.png";
-    }
-
-    }
+    Computer();
 }
 
